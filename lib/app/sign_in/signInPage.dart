b5e741edca8c009af54c9b2b63ddd6bc5ba23ecf
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-03-25 00:14:57 +0100

    NEW: Empezando con signInPage

--- lib/app/sign_in/signInPage.dart
+++ lib/app/sign_in/signInPage.dart
@@ -9,9 +9,43 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         actions: [],
         title: Text('QuizToYou Sign-in'),
+        elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.white,
+      body: _builContent(),
+    );
+  }
+
+  Widget _builContent() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100,
+              width: double.infinity,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100,
+              width: double.infinity,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100,
+              width: double.infinity,
+            ),
+          ),
+        ],
       ),
     );
   }
