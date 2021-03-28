5ce433adbe2651759ea5a88f3b02e443f468c6f5
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-03-28 22:29:13 +0200

    CHANGE: Configurando Firebase en iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quiztoyou/app/sign_in/signInPage.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'QuizToYou App',
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      theme: ThemeData(primarySwatch: Colors.pink),
       home: SignInPage(),
     );
   }
