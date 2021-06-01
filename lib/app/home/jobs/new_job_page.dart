b5bb0c8aab6dc2d46c4b0400c7cce7ea487cf775
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-06-02 00:47:45 +0200

    NEW: Starting new widget FORM for creating JOBS

--- lib/app/home/jobs/new_job_page.dart
+++ lib/app/home/jobs/new_job_page.dart
@@ -17,13 +17,97 @@ class NewJobPage extends StatefulWidget {
 }
 
 class _NewJobPageState extends State<NewJobPage> {
+  GlobalKey<FormState> _formKey = GlobalKey();
+  String? _name;
+  int? _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState!;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('Form Saved');
+      print('Name: $_name');
+      print('Rate: $_ratePerHour');
+      // TODO: Submit data to Firestore
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
         title: Text('New Job'),
+        actions: [
+          TextButton(
+            onPressed: _submit,
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: _buildContents(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContents() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _buildForm(),
+          ),
+        ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildFormChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Job name'),
+        onSaved: (value) => _name = value,
+        validator: (value) => value!.isNotEmpty ? null : 'Name canot be empty',
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Rate per hour'),
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+        onSaved: (value) {
+          print('Value: $value');
+          _ratePerHour = 0;
+          if (value != '') {
+            _ratePerHour = int.parse(value!);
+          }
+        },
+      ),
+    ];
   }
 }
